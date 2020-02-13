7043a52fc0ca55f5857662251db07285028deed9
Vandana Sivadasan <sivadasanvandana62@gmail.com>
2020-02-13 15:00:39 -0600

    my changes

---
+++ index.js
@@ -0,0 +1,78 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+        type: "input",
+        name: "image",
+        message: ""
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "Vandana Sivadasan"
+      },
+      {
+        type: "input",
+        name: "location",
+        message: "Overland Park,Kansas"
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "https://github.com/Vandana-Sivadasan"
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub Username"
+      },
+      {
+        type: "input",
+        name: "linkedin",
+        message: "https://www.linkedin.com/in/vandana-sivadasan-61aba5198/."
+      }
+    ]);
+  }
+    
+function generateHTML(answers) {
+    return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+      <p class="lead">I am from ${answers.location}.</p>
+      <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+      <ul class="list-group">
+        <li class="list-group-item">My GitHub username is ${answers.github}</li>
+        <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+      </ul>
+    </div>
+  </div>
+  </body>
+  </html>`;
+  }
+  promptUser()
+  .then(function(answers) {
+    const html = generateHTML(answers);
+
+    return writeFileAsync("index.html", html);
+  })
+  .then(function() {
+    console.log("Successfully wrote to index.html");
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
